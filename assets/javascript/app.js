35ec222e46cecd62a949a702be8a338afe01c2b8
copperdime <mrs.kelly@gmail.com>
2017-05-25 21:40:23 -0700

    Submitting game changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,15 +12,12 @@
 // 				Good Job!
 // 			if button === false;
 // 				You lose!
-// var audio = new Audio("raven.mp3");
-
 
 setTimeout(tenSeconds, 1000 * 10);
 setTimeout(timeUp, 1000 * 20);
+// setTimeout(countdown, 1000 * 1);
 
 function tenSeconds() {
-  // in the element with an id of time-left add an h2 saying About 5 Seconds Left!
-  // console log 5 seconds left
   $("#time-left").html("<h4>About 10 Seconds Left!</h4>");
   console.log("10 seconds left");
 }
@@ -34,111 +31,107 @@ function timeUp() {
   //  The following line will play the audio file we linked to above:
   // audio.play();
 }
+
 	/**
-	Questions:
-	*
-	Questions are populated using JQuery. Use an iterative loop to control question displayed
+	Questions are populated using JQuery. Use an iterative loop
+	to control question displayed
 	*/
+	var questionArr =
+	
+	[
+			question1 = {
+			question: "What color are my eyes?",
+			answer: ["Brown", "Blue", "Hazel"],
+			correctAnswer: 'Brown'
+		},
+			question2 = {
+			question: "How tall am I?",
+			answer: ["5'2", "5'5", "5'9"],
+			correctAnswer: "5'2"
+		},
+			question3 = {
+			question: "What color is my hair?",
+			answer: ["Red", "Brown", "Blond"],
+			correctAnswer: "Brown"
+		},
+			question4 = {
+			question: "How old am I?",
+			answer: ["25", "30", "36"],
+			correctAnswer: "36"
+		},
+			question5 = {
+			question: "What's my weight?",
+			answer: ["125lbs", "120lbs", "150lbs"],
+			correctAnswer: "125lbs"
+		},
+			question6 = {
+			question: "What's my ethncity?",
+			answer: ["Black", "White", "Asian"],
+			correctAnswer: "Black"
+		},
+			question7 = {
+			question: "What's my shoe size?",
+			answer: ["8", "7", "6"],
+			correctAnswer: "8"
+		},
+			question8 = {
+			question: "What's my favorite food?",
+			answer: ["Chocolate", "Bread", "Carrots"],
+			correctAnswer: "Chocolate"
+		},
+			question9 = {
+			question: "What's my profession?",
+			answer: ["Sales Associate", "Engineer", "Medical Doctor"],
+			correctAnswer: "Engineer"
+		},
+			question10 = {
+			question: "Where do I live?",
+			answer: ["Chicago, IL", "New York, NY", "San Jose, CA"],
+			correctAnswer: "San Jose, CA"
+		}
+];
 
-	var question1 = {
-		question: "What color are my eyes?",
-		answer: ["brown", "blue", "green"]
-	};
-	var question2 = {
-		question: "How tall am I?",
-		answer: "5'2"
-	};
-	var question3 = {
-		question: "What color is my hair?",
-		answer: "black"
-	};
-	var question4 = {
-		question: "How old am I?",
-		answer: "36"
-	};
-	var question5 = {
-		question: "Guess my weight?",
-		answer: "NOYB"
-	};
+			var randQ = Math.floor(Math.random() * ((questionArr.length)));
+			
+  			var postQ = questionArr[randQ].question;
+  			var postA = questionArr[randQ].answer;
+  			var postC = questionArr[randQ].correctAnswer;
+  			console.log(randQ);
+  			console.log(postQ);
+  			console.log(postA);
+  	
 
-	var question6 = {
-		question: "Guess my ethncity?",
-		answer: "black"
-	};
-	var question7 = {
-		question: "Guess my shoe size?",
-		answer: "8"
-	};
-	var question8 = {
-		question: "What's my favorite food?",
-		answer: "Chocolate"
-	};
-	var question9 = {
-		question: "Guess my profession?",
-		answer: "Engineer"
-	};
-	var question10 = {
-		question: "Where do I live?",
-		answer: "San Jose"
-	};
+			$("#questionAsked").html(postQ);
 
-	var questions = [question1, question2, question3, question4, question5,
-	question6, question7, question8, question9, question10]; 
-	// individual questions
-	var postQ_i0 = questions[0].question;
-	var postQ_i1 = questions[1].question;
-	var postQ_i2 = questions[2].question;
-	var postQ_i3 = questions[3].question;
-	var postQ_i4 = questions[4].question;
-	var postQ_i5 = questions[5].question;
-	var postQ_i6 = questions[6].question;
-	var postQ_i7 = questions[7].question;
-	var postQ_i8 = questions[8].question;
-	var postQ_i9 = questions[9].question;
+	  		$("#answers_0").html(postA[0]);
+	  		$("#answers_1").html(postA[1]);
+	  		$("#answers_2").html(postA[2]);
+  		
 
-	// individual answers to the individual questions
-	var postA_i0 = questions[0].answers;
-	var postA_i1 = questions[1].answers;
-	var postA_i2 = questions[2].answers;
-	var postA_i3 = questions[3].answers;
-	var postA_i4 = questions[4].answers;
-	var postA_i5 = questions[5].answers;
-	var postA_i6 = questions[6].answers;
-	var postA_i7 = questions[7].answers;
-	var postA_i8 = questions[8].answers;
-	var postA_i9 = questions[9].answers;
+  			function winOrLoss(){
+  				console.log($(this).text(), postC);
+  				// if user selects the correct button they win
+  				if (postC === $(this).text()){
+  					// var trueOrFalse = true;
+  					console.log("true");
+  					// wins += wins;
+  					return;
+  				}
+  				// if time runs out before user selects a button, they lose
+  				else if(timeUp === 0){
+  					// losses += losses;
+  					console.log("Times up. You lost!");
+  					return;
+  				}
+  				// if user selects a button but it is not the correct one, they lose
+  				else{
+  					// trueOrFalse = false;
+  					console.log("false");
+  					// losses += losses;
+  					return;
+  				}
+  			}
 
+  			$(".btn").on("click", winOrLoss);
 
-	var postQ = [postQ_i0, postQ_i1, postQ_i2, postQ_i3, postQ_i4,
-	postQ_i5, postQ_i6, postQ_i7, postQ_i8, postQ_i9];
-
-	var randQ = postQ[Math.floor(Math.random()*postQ.length)];
-	// var randA = postA[Math.floor(Math.random()*postA.length)];
-
-  		for (var i = 0 ; i < questions.length; i++){
-  				var postQ = questions[i].question;
-  			$("#questionAsked").html(randQ);
-  			// .appendTo('#questionAsked' + i);
-  		}
-  		// for $("<button>"){
-
-  		// }
-  		// var answer = question1.answer[i];
-//   		console.log(questions[0].question);
-// //   			var image = $("<img>")
-//   				.attr('class', 'crystal')
-//   				.attr('width', 240)
-//   				.attr('height', 240)
-//   				.attr('src', images[i])
-//   				.attr('data-randnum', randomNumber)
-//   				.appendTo('#image-' + i);
-//   		}
-
-// 	Each question is a key and has an answer value.
-// 	if value of button = value of key
-// 		answer if true;
-// 	else 
-// 		answer if false;
-
-// *
 
